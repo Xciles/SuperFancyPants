fee5e4b529c387a26ec37518b478ed3e7fbe4e47
Xciles <xciles@gmail.com>
2017-10-30 12:54:37 +0100

    initial game

--- SuperFancyPants/Program.cs
+++ SuperFancyPants/Program.cs
@@ -2,43 +2,59 @@
 
 namespace SuperFancyPants
 {
+    public class Game
+    {
+        private static string _name;
+        private static string _house;
+        private static string _street;
+        private static string _level;
+
+        public static void StartGame()
+        {
+            Initialize();
+
+            Start();
+
+            End();
+        }
+
+        private static void End()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("-----       END     ------");
+            Console.WriteLine("--------------------------");
+        }
+
+        private static void Start()
+        {
+            Console.WriteLine("Wat is je naam?");
+            _name = Console.ReadLine();
+            Console.WriteLine("Wat is je house?");
+            _house = Console.ReadLine();
+            Console.WriteLine("Wat is je street?");
+            _street = Console.ReadLine();
+            Console.WriteLine("Wat is je level?");
+            _level = Console.ReadLine();
+
+            Console.WriteLine($"Hallo {_name}, Je woont op {_street} en je huis staat op {_house}. " +
+                              $"Je level is {_level}");
+        }
+
+        private static void Initialize()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("-----SuperFancyPants------");
+            Console.WriteLine("--------------------------");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            PrintMethod();
+            Game.StartGame();
 
             Console.ReadKey();
-        }
-
-        public static void PrintMethod()
-        {
-            Console.WriteLine("Vanuit de print methode");
-
-            string result = Console.ReadLine();
-
-            Console.WriteLine(result);
-
-            Console.WriteLine("Dit was het resultaat: " + result);
-            Console.WriteLine(String.Format("Dit was het resultaat: {0}", result));
-            Console.WriteLine($"Dit was het resultaat: {result}");
-
-            string multiLine = @"Hallo
-                                    Multi line";
-
-            string s = "C:\\Projects\\Temp\\";
-            string s2 = @"C:\Projects\Temp\";
-
-            Console.WriteLine(multiLine);
-
-            string @string;
-            int i;
-            float f;
-            double d;
-            bool b;
-            long l;
         }
     }
 }
